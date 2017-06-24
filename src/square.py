c8eae5933177e73f9be5a9930fb213a64e2c1cac
Jrming Lai <jmlai.me92g@g2.nctu.edu.tw>
2017-06-24 10:58:08 +0800

    added a second function for square

--- src/square.py
+++ src/square.py
@@ -1,8 +1,12 @@
-def my_square(x):
+def my_square(y):
     """takes a value and return the squared values.
 
     uses the ** operator
     """
-    return (x ** 2)
+    return (y ** 2)
 
-print(my_square(4))
+def my_square2(x):
+    return(x * x)
+
+print(my_square(42))
+print(my_square2(42))
